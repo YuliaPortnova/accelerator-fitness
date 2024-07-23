8592af2233fa56bf84c0c125213c950b0a1f3f57
YuliaPortnova <yuyul@yandex.ru>
2024-07-23 08:31:04 +0300

    feat: завершает валидацию формы

--- source/js/validation.js
+++ source/js/validation.js
@@ -1,40 +1,34 @@
 const form = document.querySelector('.form__form');
-const nameInput = form.querySelector('.form__input--text');
-const telInput = form.querySelector('.form__input-tel');
+const inputs = form.querySelectorAll('.form__input');
 const submitButton = form.querySelector('.form__button');
 
-nameInput.addEventListener("input", function (event) {
-  if (nameInput.validity.valid) {
-    nameInput.classList.remove('form__input--error');
-  }
-});
+const messages = {
+  required: 'Это обязательное поле',
+  error: 'Некорректные данные',
+};
 
-telInput.addEventListener("input", function (event) {
-  if (telInput.validity.valid) {
-    telInput.classList.remove('form__input--error');
+const showMessage = (isValueMissing, input) => {
+  let message = messages.error;
+  if (isValueMissing) {
+    message = messages.required;
   }
-});
+  input.nextElementSibling.textContent = message;
+  input.classList.add('form__input--error');
+}
+
+inputs.forEach(input => {
+  input.addEventListener("input", function (event) {
+    if (input.validity.valid) {
+      input.classList.remove('form__input--error');
+    }
+  });
+})
 
 form.addEventListener("submit", (event) => {
-  if (!nameInput.validity.valid) {
-    nameInput.classList.add('form__input--error');
-    event.preventDefault();
-  }
-  if (!telInput.validity.valid) {
-    telInput.classList.add('form__input--error');
-    event.preventDefault();
-  }
+  inputs.forEach(input => {
+    if (!input.validity.valid) {
+      showMessage(input.validity.valueMissing, input);
+      event.preventDefault();
+    }
+  })
 });
-
-// const form = document.querySelector('.form__form');
-// const inputs = form.querySelectorAll('.form__input');
-
-// inputs.forEach(input => {
-//     input.addEventListener('invalid', () => {
-//         input.classList.add('form__input--error');
-//     });
-
-//     input.addEventListener('input', () => {
-//         input.classList.remove('form__input--error');
-//     });
-// });
