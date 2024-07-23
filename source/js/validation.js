7d12880ce3cbf789f6aca77e1ff7ba0c59c424d2
YuliaPortnova <yuyul@yandex.ru>
2024-07-23 08:33:44 +0300

    fix: исправляет ошибки по правилам eslint

--- source/js/validation.js
+++ source/js/validation.js
@@ -1,6 +1,5 @@
 const form = document.querySelector('.form__form');
 const inputs = form.querySelectorAll('.form__input');
-const submitButton = form.querySelector('.form__button');
 
 const messages = {
   required: 'Это обязательное поле',
@@ -14,21 +13,21 @@ const showMessage = (isValueMissing, input) => {
   }
   input.nextElementSibling.textContent = message;
   input.classList.add('form__input--error');
-}
+};
 
-inputs.forEach(input => {
-  input.addEventListener("input", function (event) {
+inputs.forEach((input) => {
+  input.addEventListener('input', () => {
     if (input.validity.valid) {
       input.classList.remove('form__input--error');
     }
   });
-})
+});
 
-form.addEventListener("submit", (event) => {
-  inputs.forEach(input => {
+form.addEventListener('submit', (event) => {
+  inputs.forEach((input) => {
     if (!input.validity.valid) {
       showMessage(input.validity.valueMissing, input);
       event.preventDefault();
     }
-  })
+  });
 });
