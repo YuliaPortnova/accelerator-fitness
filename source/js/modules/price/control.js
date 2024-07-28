f5d2d9de631d6999c131d2a6a0af2741a9fc0121
YuliaPortnova <yuyul@yandex.ru>
2024-07-28 15:15:50 +0300

    refactor: выносит функции отрисовки во view

--- source/js/modules/price/control.js
+++ source/js/modules/price/control.js
@@ -2,20 +2,16 @@ import { prices } from './model';
 import { renderPrice } from './view';
 
 const tabButtonsElements = document.querySelectorAll('.price__tab-button');
-const pricesElements = document.querySelectorAll('.price__rate-price');
 
 let currentIndex = 0;
 
 const initPrices = () => {
-  tabButtonsElements[currentIndex].classList.add('price__tab-button--active');
+  renderPrice(prices[currentIndex], currentIndex);
 
   tabButtonsElements.forEach((buttonElement, index) => {
     buttonElement.addEventListener('click', () => {
       currentIndex = index;
-      tabButtonsElements.forEach((element, elementIndex) => {
-        element.classList.toggle('price__tab-button--active', elementIndex === currentIndex);
-      });
-      renderPrice(pricesElements, prices[currentIndex]);
+      renderPrice(prices[currentIndex], currentIndex);
     });
   });
 };
--- source/js/modules/price/view.js
+++ source/js/modules/price/view.js
@@ -1,9 +1,15 @@
-const renderPrice = (elements, prices) => {
-  elements.forEach((element, index) => {
+const tabButtonsElements = document.querySelectorAll('.price__tab-button');
+const pricesElements = document.querySelectorAll('.price__rate-price');
+
+const renderPrice = (prices, currentIndex) => {
+  tabButtonsElements.forEach((element, elementIndex) => {
+    element.classList.toggle('price__tab-button--active', elementIndex === currentIndex);
+  });
+
+  pricesElements.forEach((element, index) => {
     element.textContent = prices[index];
     element.dataset.price = prices[index];
   });
 };
 
 export { renderPrice };
-
